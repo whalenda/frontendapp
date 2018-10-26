6c06286498b6d393c67a7b252185f87ab0ef382c
= <whalenda@umich.edu>
2018-10-26 12:14:11 -0400

    Home screen

--- app/src/main/java/com/gulogulo/starterapp/MainActivity.java
+++ app/src/main/java/com/gulogulo/starterapp/MainActivity.java
@@ -28,74 +28,12 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
-    private ArrayList<Chatt> chattArrayList;
-    private ChattAdapter chattAdapter;
-    private SwipeRefreshLayout refreshContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        chattArrayList = new ArrayList<Chatt>();
-        chattAdapter = new ChattAdapter(this, chattArrayList);
-        ListView lView = (ListView) findViewById(R.id.chattListView);
-        lView.setAdapter(chattAdapter);
-
-        refreshContainer = (SwipeRefreshLayout) findViewById(R.id.refreshContainer);
-        refreshContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                refreshTimeline();
-            }
-        });
-
-        refreshTimeline();
     }
 
-    private void refreshTimeline() {
-        chattAdapter.clear();
-        RequestQueue queue = Volley.newRequestQueue(this);
-        final String url = "http://159.203.172.151/getchatts/";
-
-        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>()
-                {
-                    @Override
-                    public void onResponse(JSONObject response)  {
-                        try {
-                            JSONArray array = response.getJSONArray("chatts");
-                            for (int i = 0; i < array.length(); i++) {
-                                String username = array.getJSONArray(i).getString(0);
-                                String message = array.getJSONArray(i).getString(1);
-                                String timestamp = array.getJSONArray(i).getString(2);
-                                chattAdapter.add(new Chatt(username, message, timestamp));
-                            }
-                        }
-                        catch (JSONException e) {
-                        }
-                        refreshContainer.setRefreshing(false);
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        refreshContainer.setRefreshing(false);
-                    }
-                }
-        );
-
-        queue.add(getRequest);
-    }
-
-
-    public void postChatt(View view) {
-        Intent intent = new Intent(this, PostActivity.class);
-        startActivity(intent);
-    }
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     public native String stringFromJNI();
 }
