03fafff91c3f23b962430bac5286f6735bb0d017
= <whalenda@umich.edu>
2018-11-07 09:06:46 -0500

    Home screen

--- app/src/main/java/com/gulogulo/starterapp/PhotoActivity.java
+++ app/src/main/java/com/gulogulo/starterapp/PhotoActivity.java
@@ -1,7 +1,9 @@
 package com.gulogulo.starterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class PhotoActivity extends AppCompatActivity {
         setContentView(R.layout.found_photo);
     }
 
-
+    public void gotodone(View view)
+    {
+        Intent intent = new Intent(PhotoActivity.this, info.class);
+        startActivity(intent);
+    }
 }
