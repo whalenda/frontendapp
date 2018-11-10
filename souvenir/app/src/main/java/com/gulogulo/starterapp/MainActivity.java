d32adfaff46dcd92badb137390dab3e69d9494ad
zyang7 <zyang7@users.noreply.github.com>
2018-11-10 12:06:02 -0500

    renamed gulo-gulo/app to gulo-gulo/souvenir to reduce confusion. When opening up Android Studio project, open the project from the gulo-gulo/souvenir folder.

---
+++ souvenir/app/src/main/java/com/gulogulo/starterapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.gulogulo.starterapp;
+
+import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static int RESULT_LOAD_IMG = 1;
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void UploadFile(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+        // Create intent to Open Image applications like Gallery, Google Photo
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+            //TODO: action
+        }
+    }
+    public native String stringFromJNI();
+    public static final int PICK_IMAGE = 1;
+}
