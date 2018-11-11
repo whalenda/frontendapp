17fb593fee9cf39cdaee01060c7f16c8dc373bd8
= <whalenda@umich.edu>
2018-11-11 18:00:32 -0500

    login

--- souvenir/app/src/main/java/com/gulogulo/starterapp/LoginActivity.java
+++ souvenir/app/src/main/java/com/gulogulo/starterapp/LoginActivity.java
@@ -306,7 +306,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
+            //
 
             try {
                 // Simulate network access.
